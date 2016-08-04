af62718f6da26bd93a6d515d1775f3ce06c40f95
Michael Mittermayr <michael@mittermayr.com>
2016-08-04 18:48:06 +0200

    lets go

---
+++ yemek_koala-master/app/controllers/facebooks_controller.rb
@@ -0,0 +1,53 @@
+class FacebooksController < ApplicationController
+  # GET /facebooks
+  # GET /facebooks.xml
+  APP_ID="1773057256267833"
+  APP_SECRET="81ea4740c07202801a7749e0ffabb771"
+  APP_CODE="XXXX"
+  SITE_URL="http://localhost:3000/facebooks/"
+  def index
+    # if session['access_token']
+    #   @face='You are logged in! <a href="facebooks/logout">Logout</a>'
+    # else
+    #   @face='<a href="facebooks/login">Login</a>'
+    # end
+  end
+
+  def login
+	# generate a new oauth object with your app data and your callback url
+		session['oauth'] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + 'callback')
+    #Koala::Facebook::OAuth.new(oauth_callback_url).
+
+		# redirect to facebook to get your code
+		redirect_to session['oauth'].url_for_oauth_code()
+
+  end
+
+  def logout
+    session['oauth'] = nil
+    session['access_token'] = nil
+    redirect_to root_path
+  end
+
+	#method to handle the redirect from facebook back to you
+	def callback
+		#get the access token from facebook with your code
+		session['access_token'] = session['oauth'].get_access_token(params[:code])
+		redirect_to facebooks_menu_path
+	end
+
+  def menu
+     @ok="Please enter your event of choice"
+     # if session['access_token']
+     #   @face='You are logged in! <a href="./logout">Logout</a>'     
+     # else
+     #   @face='<a href="/login">Login</a>'
+     # end
+  end
+
+  def new
+      event = params["event_url"].gsub(/[^\d]/, '')
+      email = params["email"]
+      EventListener.perform_async(event, email)
+  end
+end
---
+++ yemek_koala-master/app/workers/event_listener.rb
@@ -0,0 +1,43 @@
+require "koala"
+
+class EventListener
+  #include Sidekiq::Worker
+  include SuckerPunch::Job
+
+  def perform(event, email)
+    puts "Job is starting!*****************************"
+     #if session['access_token']
+        #@graph = Koala::Facebook::API.new#(session["access_token"], "81ea4740c07202801a7749e0ffabb771")
+        @oauth = Koala::Facebook::OAuth.new("1773057256267833", "81ea4740c07202801a7749e0ffabb771")
+        @oauth_token = @oauth.get_app_access_token
+        @graph = Koala::Facebook::API.new(@oauth_token)
+        
+        # Start iteration
+        while true do
+            puts "Job is running!*****************************"
+  
+            posts = @graph.get_connections(event,"feed")
+
+            @tickets = []
+
+            posts.each do |post|
+              next if post["message"].nil?
+              url = "#{(20..100).to_a.sample}% - <a href='http://www.facebook.com/#{post["id"]}'>#{post["message"]}</a>"
+
+              bid = {:link => url} if (post["message"].downcase.include?("€") || 
+                  post["message"].include?("$") ||
+                  post["message"].include?("usd") ||
+                  post["message"].include?("sell") ||
+                  post["message"].include?("offer") ||
+                  post["message"].include?("verkaufe") ||
+                  post["message"].include?("biete") ||
+                  post["message"].include?("euro"))
+
+              @tickets << bid if bid.present?
+            end
+            ActionMailer::Base.mail(:content_type => 'text/html', :from => "kaisercoins@gmail.com", :to => email, :subject => "Ticket ALERT!", :body => @tickets.map{|x| "<br>" + x[:link].html_safe}).deliver_now if @tickets.present?
+            sleep 120
+        end
+      #end
+  end
+end
